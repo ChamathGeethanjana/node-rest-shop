68bfd9290384498368f004327489425be8259ac3
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-03-16 03:16:37 +0530

    created database connection and added data saveing and accessing code.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,32 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Handle incoming GET request to /orders
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order were fetched',
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Order were fetched",
+  });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Order was created'
-    });
+router.post("/", (req, res, next) => {
+  const order = {
+    productID: req.body.productID,
+    quantity: req.body.quantity,
+  };
+  res.status(201).json({
+    message: "Order was created",
+    order: order,
+  });
 });
 
-router.get('/:orderId', (req, res, next) => {
-    res.status(201).json({
-        message: 'Order details',
-        orderId: req.params.orderId
-    });
+router.get("/:orderId", (req, res, next) => {
+  res.status(201).json({
+    message: "Order details",
+    orderId: req.params.orderId,
+  });
 });
 
-router.delete('/:orderId', (req, res, next) => {
-    res.status(201).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
+router.delete("/:orderId", (req, res, next) => {
+  res.status(201).json({
+    message: "Order deleted",
+    orderId: req.params.orderId,
+  });
 });
-
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,30 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        meessage: 'Handling GET request to /products'
+const Product = require("../models/product");
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    meessage: "Handling GET request to /products",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        meessage: "Handling POST request to /products",
+        createdProduct: result,
+      });
     })
-}) 
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        meessage: 'Handling POST request to /products'
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
     })
-}) 
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id == 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        })
-    }else{
-        res.status(200).json({
-            message: 'You passed an ID'
-        })
-    }
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
 
 module.exports = router;
