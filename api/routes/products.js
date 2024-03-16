7a5529825a990483afa208347ce642eaabf256f1
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-03-16 17:16:20 +0530

    Added delete, patch routs

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,24 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    meessage: "Handling GET request to /products",
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      //   if (docs.length >= 0) {
+      res.status(200).json(docs);
+      //   } else {
+      //     res.status(404).json({
+      //       message: "No entries found",
+      //     });
+      //   }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -46,6 +61,37 @@ router.get("/:productId", (req, res, next) => {
           .status(404)
           .json({ message: "No valid entry found for provided ID" });
       }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
     })
     .catch((err) => {
       console.log(err);
